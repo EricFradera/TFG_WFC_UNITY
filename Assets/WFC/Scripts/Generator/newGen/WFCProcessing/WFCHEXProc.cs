e1cfd362ab3f0a9a0c93a22fef6545d7146a002c
EricFradera <efradera@edu.tecnocampus.cat>
2023-06-02 13:46:04 +0200

    null pointer adjecency fix

--- Assets/WFC/Scripts/Generator/newGen/WFCProcessing/WFCHEXProc.cs
+++ Assets/WFC/Scripts/Generator/newGen/WFCProcessing/WFCHEXProc.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class WFCHEXProc : WFCAbstractProc
 {
-    private Direction[] direction = { Di };
+    private Direction[] direction = { };
     private List<WFCTile> listOfRotatedTiles;
     private bool useRotation;
 
