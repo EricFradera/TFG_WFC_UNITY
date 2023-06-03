6d567d80d43fc4262baabafc1f034cc50505d476
EricFradera <efradera@edu.tecnocampus.cat>
2023-06-03 17:14:51 +0200

    hexspawner working

--- Assets/WFC/Scripts/Generator/newGen/Spawner/WFCSpawnerHex.cs
+++ Assets/WFC/Scripts/Generator/newGen/Spawner/WFCSpawnerHex.cs
@@ -7,6 +7,8 @@ public class WFCSpawnerHex : WFCSpawnerAbstact
 {
     private Dictionary<string, Material> materials = new Dictionary<string, Material>();
     private GameObject[,] gameObjectArray;
+    private float tileOffsetX = 1f;
+    private float tileOffsetZ = 1f;
 
     public WFCSpawnerHex(Transform transform, int lineCount, float m_gridSize, float m_gridExtent) : base(transform,
         lineCount, m_gridSize, m_gridExtent)
@@ -16,16 +18,20 @@ public class WFCSpawnerHex : WFCSpawnerAbstact
 
     public override void spawnTiles(ITopoArray<WFCTile> result, bool useRotations)
     {
+        Debug.Log("Line count is: " + this.lineCount);
         GameObject tempGameObject;
         for (var x = 0; x < lineCount; x++)
-        for (var z = 0; z < lineCount; z++)
         {
-            tempGameObject =  GameObject.CreatePrimitive(PrimitiveType.Quad);
-            tempGameObject.transform.Rotate(new Vector3(90f, 0, 0));
-            tempGameObject.GetComponent<MeshRenderer>().material = genMat((WFCHEXTile)result.Get(x, z));
-            tempGameObject.transform.position = z % 2 == 0
-                ? new Vector3(x * 1, 0, z * 1)
-                : new Vector3(x * 1 + 1 / 2, 0, 1);
+            for (var z = 0; z < lineCount; z++)
+            {
+                tempGameObject = Object.Instantiate(result.Get(x, z).tileVisuals);
+                //tempGameObject.transform.Rotate(new Vector3(90f, 0, 0));
+                //tempGameObject.GetComponent<MeshRenderer>().material = genMat((WFCHEXTile)result.Get(x, z));
+                tempGameObject.transform.position = z % 2 == 0
+                    ? new Vector3(x * tileOffsetX, 0, z * tileOffsetZ)
+                    : new Vector3(x * tileOffsetX + tileOffsetX / 2, 0,z* tileOffsetZ);
+                tempGameObject.transform.parent = this.transform;
+            }
         }
     }
 
